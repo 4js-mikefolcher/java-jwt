adf7887a1536cef8b8964b1a31e16b6a5c6124e0
Mike Folcher <mifo@4js.com>
2021-03-22 10:08:43 -0400

    Add script to build jar file
    Add script jar-build.sh to build the jwt jar file

---
+++ jar-build.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+cwd="`pwd`"
+srcdir="${cwd}/src/main/java/com/fourjs/jwt"
+javafile="${srcdir}/JWebToken.java"
+
+if [ ! -d "$srcdir" ]; then
+   echo "You should be in the root git directory when running this script"
+   exit 1
+fi
+
+if [ ! -f "$javafile" ]; then
+   echo "The java source file $javafile does not exist"
+   exit 1
+fi
+
+cd "$srcdir"
+javac JWebToken.java
+
+if [ $? -ne 0 ]; then
+   echo "Compilation error occurred"
+   exit 1
+fi
+
+classfile="${srcdir}/JWebToken.class"
+if [ ! -f "$classfile" ]; then
+   echo "Compilation error occurred"
+   exit 1
+fi
+
+jarroot="${cwd}/src/main/java"
+if [ ! -d "$jarroot" ]; then
+   echo "You should be in the root git directory when running this script"
+   exit 1
+fi
+cd "$jarroot"
+jar cMf jwt-fourjs.jar META-INF/MANIFEST.MF com/fourjs/jwt/JWebToken.java
+
+if [ $? -ne 0 ]; then
+   echo "An error occurred attempting to create the jar file"
+   exit 1
+fi
+
+mv jwt-fourjs.jar "${cwd}/."
+
+echo "Jar file has been created successfully"
